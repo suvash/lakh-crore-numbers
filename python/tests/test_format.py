71c88d6e14cc959211f70f7223226bdcea50ec12
Suvash Thapaliya <suvash@gmail.com>
2021-02-23 23:50:38 +0100

    Fix grammar typos in nepali words

--- python/tests/test_format.py
+++ python/tests/test_format.py
@@ -10,7 +10,7 @@ class TestFormat(unittest.TestCase):
 
   def test_format_to_nepali_words(self):
     assert fnw(0) == "सुन्ना"
-    assert fnw(92486540) == "नौ करोड चौबिस लाख छयासी हजार पाँच सय चालिस"
+    assert fnw(92486540) == "नौ करोड चौबीस लाख छयासी हजार पाँच सय चालीस"
 
 if __name__ == '__main__':
     unittest.main()
